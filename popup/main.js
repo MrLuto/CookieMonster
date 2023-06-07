bd60557b44cb22ad1a0fd0f01bb90836b53aa1fb
MrLuto <mmudde2@gmail.com>
2023-06-07 22:39:22 +0200

    nowhere to go but up

--- popup/main.js
+++ popup/main.js
@@ -1,74 +1,84 @@
+let text = document.getElementById("status");
+let selector = document.getElementById("selector");
+let exit = document.getElementById('exit');
+let menu = document.getElementById('menu');
+let local_blocked_num = document.getElementById("local_blocked_num");
+let total_blocked_num = document.getElementById("total_blocked_num");
+let Current_Domain = document.getElementById("Current_Domain");
+
+async function Get_Current_Domain() {
+  let [tabs] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  let fqnd = tabs.url;
+  let stripprot = fqnd.split('//');
+  let dn = stripprot[1].split('/');
+  gcd = dn[0];
+  return gcd;
+}
+
+async function Set_status() {
+  //let GCD = await Get_Current_Domain();
+  //let key = GCD + "_status";
+  //let localkey = GCD + "_DeletedCookies";
+  //chrome.storage.local.get(["AllDeletedCookies"]).then((result) => {total_blocked_num.innerHTML = result.AllDeletedCookies});
+
+  if (selector.checked == true){
+    text.innerHTML = "Cookie monster is </br> sleeping";
+    local_blocked_num.innerHTML = "NO";
+    local_blocked_num.style.color = "#505050";
+    chrome.storage.local.set({ key: true }).then(() => {});
+  } else {
+    //TODO: get number of blocked cookies for this domain
+    let local_blocked_amount = "#soon";
+    //chrome.storage.local.get([`${localkey}`]).then((result) => {total_blocked_num.innerHTML = result.localkey});
+    text.innerHTML = "Cookie monster is eating all your cookies";
+    local_blocked_num.innerHTML = local_blocked_amount;
+    local_blocked_num.style.color = "#FF6600";
+    chrome.storage.local.set({ key: false }).then(() => {});
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    var exit = document.getElementById('exit');
-    exit.addEventListener('click', function() {
-        window.close();
-    });
+    exit.addEventListener('click', function() {window.close()});
+    selector.addEventListener('click', function() {Set_status()});
 
-    var menu = document.getElementById('menu');
     menu.addEventListener('click', function() {
-
-        var text = document.getElementById("status");
-        var checkBox = document.getElementById("selector");
-
         text.innerHTML = "#soon<br>a menu will be here";
-        setTimeout(function() {
-            if (checkBox.checked == true){
-                text.innerHTML = "Cookie monster is </br> sleeping";
-                local_blocked_num.innerHTML = "NO";
-                local_blocked_num.style.color = "#505050";
-      
-              } else {
-                text.innerHTML = "Cookie monster is eating all your cookies";
-                local_blocked_num.innerHTML = "24"; //TODO: get number of blocked cookies
-                local_blocked_num.style.color = "#FF6600";
-              }
-          }, 2000);
+        setTimeout(function() {Set_status()}, 2000);
     });
 
-    var selector = document.getElementById('selector');
-    selector.addEventListener('click', function() {
+    async function onload() {
+      let cookies = await chrome.cookies.getAll({});
+          let GCD = await Get_Current_Domain();
+          let key = GCD + "_status";
+          Current_Domain.innerHTML = GCD;
+          let dn_splitted = GCD.split('.');
+          const cookies_filterd = cookies.filter(cookie => cookie["domain"].includes(dn_splitted[1]));
 
-        var checkBox = document.getElementById("selector");
-        var text = document.getElementById("status");
-        var local_blocked_num = document.getElementById("local_blocked_num");
+          chrome.storage.local.get([key]).then((result) => {
+            console.log(result); //TODO: check if cookie blocking is enabled for this domain
+          });
 
-        if (checkBox.checked == true){
-          text.innerHTML = "Cookie monster is </br> sleeping";
-          local_blocked_num.innerHTML = "NO";
-          local_blocked_num.style.color = "#505050";
+          if (selector.checked == false){
+            if(cookies_filterd.length == 0){
+            }else{
 
-        } else {
-          text.innerHTML = "Cookie monster is eating all your cookies";
-          local_blocked_num.innerHTML = "24"; //TODO: get number of blocked cookies
-          local_blocked_num.style.color = "#FF6600";
-        }
-    });
+              cookies_filterd.forEach(function(cookie) {
+                chrome.cookies.remove({"url": "http://"+cookie["domain"]+cookie["path"], "name": cookie["name"]});
+              });
 
-    function onload() {
-        //TODO cookie reading for data
-        document.cookie = "LastCookieEver=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        console.log(document.cookie);
-        if (false){
-            
-        }
-        var Current_Domain = document.getElementById("Current_Domain");
-        var local_blocked_num = document.getElementById("local_blocked_num");
-        var total_blocked_num = document.getElementById("total_blocked_num");
+              chrome.storage.local.get(["AllDeletedCookies"]).then((result) => {
 
-        //TODO: check if cookie blocking is enabled for this domain
-        //TODO: get number of blocked cookies for this domain
-        //TODO: get number of blocked cookies for all domains
-    
-        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-            let fqnd = tabs[0].url;
-            let stripprot = fqnd.split('//');
-            let dn = stripprot[1].split('/');
-             Current_Domain.innerHTML = dn[0];
-            // use `url` here inside the callback because it's asynchronous!
-        });
+                if (result.AllDeletedCookies == undefined) chrome.storage.local.set({ "AllDeletedCookies": 0 }).then(() => {});
+                let localkey = GCD + "_DeletedCookies";
+                let value = result.AllDeletedCookies + cookies_filterd.length;
+                chrome.storage.local.set({ "AllDeletedCookies": value }).then(() => {});
+                chrome.storage.local.set({ localkey: value }).then(() => {});
+              });
+            }
+          }
+    Set_status();
     }
     onload();
-
-
 });
